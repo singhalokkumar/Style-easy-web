73f71055db25578b974cd297bd2788b9aa461390
Aditya Raj <ADITYARAJ61522@GMAIL.COM>
2020-05-29 02:05:36 +0530

    index page setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { TechnologyComponent } from './technology/technology.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    TechnologyComponent
   ],
   imports: [
     BrowserModule,
